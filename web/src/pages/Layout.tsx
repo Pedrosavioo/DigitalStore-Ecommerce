d693624ff26cf96623a0b809d17ca7428121a80c
Pedrosavioo <pedrosaviolima618@gmail.com>
2025-03-24 23:34:42 -0300

    add message box

--- web/src/pages/Layout.tsx
+++ web/src/pages/Layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import { Toaster } from "sonner";
+import MessageBox from "../components/MessageBox/MessageBox";
 
 const Layout = () => {
    return (
@@ -11,9 +11,8 @@ const Layout = () => {
          <Header />
          <main>
             <Outlet />
-            
             // Add component Toaster (Message)
-            <Toaster /> 
+            <MessageBox />
          </main>
          <Footer />
       </>
